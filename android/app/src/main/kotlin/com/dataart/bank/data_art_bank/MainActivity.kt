c0ea05ed3a5685c38dcbb48ab0a6c83970a21024
loboda <vlad.loboda@zutobi.com>
2023-10-02 18:55:55 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/dataart/bank/data_art_bank/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dataart.bank.data_art_bank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dashboard_tab_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class DashboardTabPage extends StatelessWidget {
+  const DashboardTabPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
